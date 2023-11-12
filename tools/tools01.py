31009fddf2b3a6936e9c8496295e1054978d16fa
Derek Ding <derekding727@gmail.com>
2023-11-12 18:52:09 +0000

    added wk5hw.ipynb, added wk6.ipynb, modified tools/tools01.py

--- tools/tools01.py
+++ tools/tools01.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# this script is developed to test the capabilities of copilot
+
+
 # this is a data clearning function
 def data_cleaning(df):
     df = df.dropna()
@@ -31,3 +34,113 @@ def data_standardization(df):
     return df   
 
 
+# write a block of code that reads the data from the file
+def data_read(file):
+    df = pd.read_csv(file)
+    return df
+
+# write a tetris game
+def tetris():
+    # define the board
+    board = np.zeros((20, 10))
+    # define the shapes
+    shapes = {
+        1: np.array([[1, 1, 1, 1]]),
+        2: np.array([[1, 1], [1, 1]]),
+        3: np.array([[1, 1, 0], [0, 1, 1]]),
+        4: np.array([[0, 1, 1], [1, 1, 0]]),
+        5: np.array([[1, 0, 0], [1, 1, 1]]),
+        6: np.array([[0, 0, 1], [1, 1, 1]]),
+        7: np.array([[0, 1, 0], [1, 1, 1]]),
+    }
+    # define the colors
+    colors = {
+        1: 'red',
+        2: 'blue',
+        3: 'green',
+        4: 'yellow',
+        5: 'purple',
+        6: 'orange',
+        7: 'pink',
+    }
+    # define the score
+    score = 0
+    # define the game over
+    game_over = False
+    # define the current shape
+    current_shape = np.random.choice(list(shapes.keys()))
+    # define the current shape color
+    current_shape_color = colors[current_shape]
+    # define the current shape
+    current_shape_matrix = shapes[current_shape]
+    # define the current shape position
+    current_shape_position = [0, 3]
+    # define the next shape
+    next_shape = np.random.choice(list(shapes.keys()))
+    # define the next shape color
+    next_shape_color = colors[next_shape]
+    # define the next shape
+    next_shape_matrix = shapes[next_shape]
+    # define the next shape position
+    next_shape_position = [0, 3]
+    # define the current shape
+    board[current_shape_position[0]:current_shape_position[0] + current_shape_matrix.shape[0], current_shape_position[1]:current_shape_position[1] + current_shape_matrix.shape[1]] = current_shape_matrix
+    # define the next shape
+    board[next_shape_position[0]:next_shape_position[0]
+            + next_shape_matrix.shape[0], next_shape_position[1]:next_shape_position[1] + next_shape_matrix.shape[1]] = next_shape_matrix
+    # print the board
+    print(board)
+    # define the game loop
+    while not game_over:
+        # get the user input
+        user_input = input('Please enter the direction you want to move: ')
+        # define the user input
+        if user_input == 'a':
+            # move the current shape to the left
+            current_shape_position[1] -= 1
+        elif user_input == 'd':
+            # move the current shape to the right
+            current_shape_position[1] += 1
+        elif user_input == 'w':
+            # rotate the current shape
+            current_shape_matrix = np.rot90(current_shape_matrix)
+        elif user_input == 's':
+            # move the current shape down
+            current_shape_position[0] += 1
+        # define the current shape
+        board[current_shape_position[0]:current_shape_position[0] + current_shape_matrix.shape[0], current_shape_position[1]:current_shape_position[1] + current_shape_matrix.shape[1]] = current_shape_matrix
+        # print the board
+        print(board)
+        # define the next shape
+        board[next_shape_position[0]:next_shape_position[0]
+                + next_shape_matrix.shape[0], next_shape_position[1]:next_shape_position[1] + next_shape_matrix.shape[1]] = next_shape_matrix
+        # print the board
+        print(board)
+        # define the current shape
+        board[current_shape_position[0]:current_shape_position[0] + current_shape_matrix.shape[0], current_shape_position[1]:current_shape_position[1] + current_shape_matrix.shape[1]] = 0
+        # define the next shape
+        board[next_shape_position[0]:next_shape_position[0]
+                + next_shape_matrix.shape[0], next_shape_position[1]:next_shape_position[1] + next_shape_matrix.shape[1]] = 0
+        # define the current shape
+        current_shape_position[0] += 1
+        # define the next shape
+        next_shape_position[0] += 1
+        # define the current shape
+        board[current_shape_position[0]:current_shape_position[0] + current_shape_matrix.shape[0], current_shape_position[1]:current_shape_position[1] + current_shape_matrix.shape[1]] = current_shape_matrix
+        # define the next shape
+        board[next_shape_position[0]:next_shape_position[0]
+                + next_shape_matrix.shape[0], next_shape_position[1]:next_shape_position[1] + next_shape_matrix.shape[1]] = next_shape_matrix
+        # print the board
+        print(board)
+        # define the current shape
+        board[current_shape_position[0]:current_shape_position[0] + current_shape_matrix.shape[0], current_shape_position[1]:current_shape_position[1] + current_shape_matrix.shape[1]] = 0
+        # define the next shape
+        board[next_shape_position[0]:next_shape_position[0]
+                + next_shape_matrix.shape[0], next_shape_position[1]:next_shape_position[1] + next_shape_matrix.shape[1]] = 0
+        # define the current shape
+        current_shape_position[0] -= 1
+        # define the next shape
+        next_shape_position[0] -= 1
+        # define the current shape
+        board[current_shape_position[0]:current_shape_position[0] + current_shape_matrix.shape[0], current_shape_position[1]:current_shape_position[1] + current_shape_matrix.shape[1]] = current_shape_matrix
+        
